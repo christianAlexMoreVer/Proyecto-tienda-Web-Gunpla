bcab691f7c64118718f57cdc41a16771915b68c8
christianAlexMoreVer <escorpion2001chris@gmail.com>
2021-11-26 19:20:36 +0000

    trabajando en el frondtend

--- frontend/src/app/seeker/seeker.page.ts
+++ frontend/src/app/seeker/seeker.page.ts
@@ -1,7 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Gunpla } from '../models/gunplas';
-import { GunplaService } from '../services/gunpla.service';
 
 @Component({
   selector: 'app-seeker',
@@ -13,7 +12,7 @@ export class SeekerPage implements OnInit {
   private UsuarioLoggedId: number;
   public gunplasByName: Array<Gunpla> = [];
 
-  constructor(private gunplaService: GunplaService) { }
+  constructor() { }
 
   ngOnInit() {
 
@@ -27,10 +26,7 @@ export class SeekerPage implements OnInit {
     let val = ev.target.value;
 
     if(val && val.trim() != ''){
-      this.gunplaService.getGunplaByName(val).subscribe((g: Array<Gunpla>) => {
-        this.gunplasByName = g;
-        document.getElementById("results").style.display="flex";
-      })
+      
     }
     
     else{
