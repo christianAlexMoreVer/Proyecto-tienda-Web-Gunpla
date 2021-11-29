f612ed3203ab7e59418b9e46891a4c8394f50efd
christianAlexMoreVer <escorpion2001chris@gmail.com>
2021-11-29 15:37:59 +0000

    actualizacion de imagenes

--- frontend/src/app/seeker/seeker.page.ts
+++ frontend/src/app/seeker/seeker.page.ts
@@ -35,6 +35,7 @@ export class SeekerPage implements OnInit {
                gunplaByName(nombre: \"`+val+`\"){
                 nombre
                 descripcion
+                imgFileName
           }
         }`
         })
